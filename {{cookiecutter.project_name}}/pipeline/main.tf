72bad0f65febcb2f025bf853f6ad2212d52693f8
Viacheslav Klovan <vyacheslav.klyovan@tivo.com>
2024-12-07 07:23:05 +0100

    Moved terraform state to S3 for pipeline tr module

--- {{cookiecutter.project_name}}/pipeline/main.tf
+++ {{cookiecutter.project_name}}/pipeline/main.tf
@@ -9,3 +9,13 @@ module "codepipeline" {
   secret_name  = "{{ cookiecutter.github_token_aws_secret }}"
   github_repo  = "{{ cookiecutter.github_owner }}/{{ cookiecutter.github_project_name }}"
 }
+
+terraform {
+  backend "s3" {
+    bucket         = "vkdev1-terraform-state"
+    key            = "pipeline/{{ cookiecutter.microservice_name }}/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "vkdev1-terraform-locks"
+    encrypt        = true
+  }
+}
